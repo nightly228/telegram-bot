b141a170073e83e4aef8f7df0bdd2dee0b402781
Elizaveta Cherkashina <134769815+nightly228@users.noreply.github.com>
2024-12-16 04:20:51 +0300

    Update main.py

--- venv/main.py
+++ venv/main.py
@@ -1,14 +1,28 @@
-from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types.web_app_info import WebAppInfo
+import telebot
+from telebot import types
 
 bot = telebot.TeleBot('7846130427:AAHt5Whc5HOdxDS2DRjmw34ltLIrpsr_3o8')
 
-@bot.message_handlers(commands=['start']) #декоратор
-async def start(message: types.Message):
-    markup = types.ReplyKeyboardMarkup()
-    markup.add(types.KeyBoardButton('Открыть веб-страницу', web_app=WebAppInfo(url='http://')))
-    await message.answer('Привет', reply_markup=markup)
+@bot.message_handler(content_types=['start']) #декоратор
+def main(message):
+    #bot.send_message(message.chat.id, 'Доброго времени суток!'
+                                      #' Этот бот поможет Вам с выбором эко-подарка и поможет узнать что-то новое, оставайтесь с нами!'
+                                      #' Вот список возможных команд:')
+    #file = open('.\kk.jpeg', 'rb')
+    #bot.send_photo(message.chat.id, file)
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.KeyboardButton('Наличие')
+    btn2 = types.KeyboardButton('Под заказ')
+    markup.row(btn1, btn2)
+    btn3 = types.KeyboardButton('Интересные факты')
+    markup.row(btn3)
 
 
+@bot.message_handler(commands=['help','помощь','инфо']) #декоратор
+def main(message):
+    bot.send_message(message.chat.id, 'Доброго времени суток!'
+                                      ' Этот бот поможет Вам с выбором эко-подарка и поможет узнать что-то новое, оставайтесь с нами!'
+                                      ' Вот список возможных команд:')
 
-executor.start_polling(dp)
+
+bot.polling(none_stop=True)
